ef2b8e25a21844abfbf4e968d39b7e4b80e50998
yanwen <nicedays233@163.com>
2023-10-15 00:17:55 -0500

    finish 20 questions.

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -6,18 +6,17 @@ fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
     // Cargo do it.
-    println!("cargo:rerun-if-changed=exercises/tests/test7.rs");
     let timestamp =  std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
+    // std::env::set_var("FOO", timestamp);
     println!("cargo:rustc-env=TEST_FOO={}", timestamp);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    println!("cargo:{}", your_command);
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 
 
 }
